69aa3131dec2694fe3527028091c87971322754f
moddedmcplayer <76910334+moddedmcplayer@users.noreply.github.com>
2023-01-06 21:32:27 +0100

    Initial commit

---
+++ InstallScriptDemoPlugin/Plugin.cs
@@ -0,0 +1,10 @@
+﻿namespace InstallScriptDemoPlugin
+{
+    using PluginAPI.Core.Attributes;
+
+    public class Plugin
+    {
+        [PluginEntryPoint("InstallScriptDemoPlugin", "1.0.0", "Description", "moddedmcplayer")]
+        void Enabled(){}
+    }
+}
